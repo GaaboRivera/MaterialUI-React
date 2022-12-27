59f94a037ad0ab52428a5276dd481ff2d25510c3
GaaboRivera <gabo.grm27@gmail.com>
2022-12-27 17:39:57 -0600

    formik login

--- src/helpers/validateForm.ts
+++ src/helpers/validateForm.ts
@@ -1,6 +1,10 @@
-import * as yup from "yup"
+import * as yup from 'yup'
 
 export const LoginValidate = yup.object().shape({
-  username: yup.string().trim().required("El username es requerido"),
-  password: yup.string().trim().required("El password es requerido"),
+  username: yup.string().trim().required('El username es requerido'),
+  password: yup
+    .string()
+    .trim()
+    .required('El password es requerido')
+    .min(6, 'Password should be of minimum 8 characters length'),
 })
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,61 +1,76 @@
 import React from 'react'
-import { Container, Button, Grid, Paper, Box, Typography, TextField } from '@mui/material';
-import { useNotification } from '../../context/notification.context';
-import { LoginValidate } from '../../helpers/validateForm';
+import {
+  Container,
+  Button,
+  Grid,
+  Paper,
+  Box,
+  Typography,
+  TextField,
+} from '@mui/material'
+import { useNotification } from '../../context/notification.context'
+import { LoginValidate } from '../../helpers/validateForm'
+import { useFormik } from 'formik'
 type LoginType = {
-  username: string,
-  password: string,
+  username: string
+  password: string
 }
 const LoginPage: React.FC<{}> = () => {
-  const { getError, getSuccess } = useNotification()
-
-  const [loginData, setLoginData] = React.useState<LoginType>({
-    username: "",
-    password: ""
+  const { getSuccess } = useNotification()
+  const formik = useFormik<LoginType>({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: LoginValidate,
+    onSubmit: (values: LoginType) => {
+      getSuccess(JSON.stringify(values))
+    },
   })
-
-  const dataLogin = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginData({ ...loginData, [e.target.name]: e.target.value })
-  }
-
-  const handleSubmit = (e: React.FormEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    LoginValidate.validate(loginData).then(() => {
-      getSuccess(JSON.stringify(loginData))
-    }).catch((error) => {
-      getError(error.message)
-    })
-  }
 
   return (
     <Container maxWidth="sm">
-      <Grid container
+      <Grid
+        container
         direction="column"
-        alignItems={"center"}
+        alignItems={'center'}
         justifyContent="center"
-        sx={{ minHeight: "100vh" }}
+        sx={{ minHeight: '100vh' }}
       >
         <Grid item>
-          <Paper sx={{ padding: "1.2em", borderRadius: "0.5em" }}>
-            <Typography sx={{ mt: 1, mb: 1 }} variant='h4'>Iniciar Sesion</Typography>
-            <Box component={"form"} onSubmit={handleSubmit}>
+          <Paper sx={{ padding: '1.2em', borderRadius: '0.5em' }}>
+            <Typography sx={{ mt: 1, mb: 1 }} variant="h4">
+              Iniciar Sesion
+            </Typography>
+            <Box component={'form'} onSubmit={formik.handleSubmit}>
               <TextField
-                name='username'
-                type={"text"}
+                name="username"
+                type={'text'}
                 fullWidth
                 label="email"
                 sx={{ mt: 2, mb: 1.5 }}
-                onChange={dataLogin}
+                value={formik.values.username}
+                onChange={formik.handleChange}
+                error={formik.touched.username && Boolean(formik.errors.username)}
+                helperText={formik.touched.username && formik.errors.username}
               />
               <TextField
-                name='password'
-                type={"password"}
+                name="password"
+                type={'password'}
                 fullWidth
                 label="password"
                 sx={{ mt: 1.5, mb: 1.5 }}
-                onChange={dataLogin}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
               />
-              <Button fullWidth type="submit" variant='contained' sx={{ mt: 1.5, mb: 2 }}>
+              <Button
+                fullWidth
+                type="submit"
+                variant="contained"
+                sx={{ mt: 1.5, mb: 2 }}
+              >
                 Iniciar Sesion
               </Button>
             </Box>
